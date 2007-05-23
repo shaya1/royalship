bfb84fb2ab3272b84d909f8335203e7d8670e166
sloonz <sloonz@dd8af200-2031-0410-9dd6-a94ec5e025bc>
2007-05-23 12:08:15 +0000

    add UJoueur.pas

---
+++ UJoueur.pas
@@ -0,0 +1,39 @@
+// ~ master: bataillenavale.lpr
+
+unit UJoueur;
+
+interface
+
+uses Affichage;
+
+type IJoueur = class
+	fAff: TForm1;
+	
+	constructor Create(aAff: TForm1); virtual;
+	destructor Destroy; virtual;
+	
+	function Jouer: TCase; virtual; abstract;
+	
+	// Résultat de notre coup précédent
+	procedure ToucheAdversaire; virtual; abstract; // Adversaire touché
+	procedure CouleAdversaire; virtual; abstract; // Adversaire coulé
+	procedure Rate; virtual; abstract; // Coup à l'eau
+	
+	// Résultat du coup de l'adversaire
+	procedure ToucheJoueur(coup: TCase); virtual; abstract; // On a été touché
+	procedure CouleJoueur(coup: TCase); virtual; abstract; // On a été coulé
+end;
+
+implementation
+
+constructor IJoueur.Create(aAff: TForm1);
+begin
+	fAff := aAff;
+end;
+
+destructor IJoueur.Destroy;
+begin
+	
+end;
+
+end.
