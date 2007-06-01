46f49fb51fe69a418c0a1c472fb7341c11fd9686
sloonz <sloonz@dd8af200-2031-0410-9dd6-a94ec5e025bc>
2007-06-01 14:15:58 +0000

    cosmetic changes

--- uiarbitre.pas
+++ uiarbitre.pas
@@ -11,13 +11,9 @@ Uses SysUtils;
 type TCase = array[0..1] of integer;
 
 Type Iarbitre=Class
-     //attributs
-     //methodes
      Constructor Create();virtual;abstract;
      Destructor  Destroy();virtual;abstract;
      Procedure Tour(); virtual;abstract;
-     
-     
 end;
 
 implementation
